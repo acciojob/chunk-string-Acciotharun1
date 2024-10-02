83cf0bd6ac48b13768f5e9169173fe25ce422e0a
Acciotharun1 <166792506+Acciotharun1@users.noreply.github.com>
2024-10-02 10:04:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function stringChop(str, size) {
   // your code here
+	chunkedarr=[];
+    let l=str.length;
+    if(str==""){
+        return chunkedarr;
+    }
+        for(let i=0;i<l;i+=size){
+            chunkedarr.push(str.slice(i,i+size));
+        }
+    
+    return chunkedarr.join(", ");
 }
 
 // Do not change the code below
